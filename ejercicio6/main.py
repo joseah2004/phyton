0fc49b8258c8a0fa48d6f28a65dd0a4ae5a5d706
joseah2004 <joseah2004@gmail.com>
2025-11-28 20:22:19 +0100

    ej6

---
+++ ejercicio6/main.py
@@ -0,0 +1,16 @@
+# Crea utilidades.py con 3 funciones:
+# limpar_texto(texto): 
+#es mayor de esdad(edad):
+#formatearusuario
+
+# crearmain.py que las use 
+# empezar a modularizar
+
+import utilidad
+
+nombre= "an"
+edad = 33
+
+print(utilidad.esmayordeedad(edad))
+print(utilidad.limpiartexto(nombre))
+print(utilidad.formatearusuario(edad, nombre))
---
+++ ejercicio6/utilidad.py
@@ -0,0 +1,8 @@
+def limpiartexto(texto):
+    return ""
+
+def esmayordeedad(edad):
+    return edad >= 18
+
+def formatearusuario(nombre, edad):
+     return f"{nombre} - {edad} años"
